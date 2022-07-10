73a8eb99b2598b9006e0864155d6eca82ab0c674
Usmonbek Rustamov <75022353+usmonbek-umarbekovich@users.noreply.github.com>
2022-07-10 22:36:43 +0200

    created routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,26 @@
+import { Routes, Route } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import Stack from 'react-bootstrap/Stack';
+import UserInfoProvider from './contexts/userInfoContext';
+import Header from './components/Header';
+import Dashboard from './pages/Dashboard';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import 'react-toastify/dist/ReactToastify.css';
+
 function App() {
   return (
-    <div>
-      Hello World
-    </div>
+    <Stack className="min-vh-100">
+      <UserInfoProvider>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+        <ToastContainer />
+      </UserInfoProvider>
+    </Stack>
   );
 }
 
