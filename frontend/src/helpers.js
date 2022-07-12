667e306b108a411b8569aa73788589bf9703d4b5
Usmonbek Rustamov <75022353+usmonbek-umarbekovich@users.noreply.github.com>
2022-07-12 08:37:38 +0200

    added capitalize helper

--- frontend/src/helpers.js
+++ frontend/src/helpers.js
@@ -1,5 +1,12 @@
 export const getFullName = name => `${name.first} ${name.last}`;
 
+export const capitalize = str => {
+  return str
+    .split(' ')
+    .map(word => `${word[0].toUpperCase()}${word.slice(1).toLowerCase()}`)
+    .join(' ');
+};
+
 export const formatTime = rawTime => {
   const parsed = new Date(rawTime);
   const intl = new Intl.DateTimeFormat([], {
