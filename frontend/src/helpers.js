1bc97e9aa27dc04f515374dd9c0bf8b0883fa3ea
Usmonbek Rustamov <75022353+usmonbek-umarbekovich@users.noreply.github.com>
2022-07-16 23:39:18 +0200

    configurable date style

--- frontend/src/helpers.js
+++ frontend/src/helpers.js
@@ -2,17 +2,15 @@ export const getFullName = name => `${name.first} ${name.last}`;
 
 export const capitalize = str => {
   if (!str) return str;
-  
+
   return str
     .split(' ')
     .map(word => `${word[0].toUpperCase()}${word.slice(1).toLowerCase()}`)
     .join(' ');
 };
 
-export const formatTime = rawTime => {
+export const formatTime = (rawTime, dateStyle) => {
   const parsed = new Date(rawTime);
-  const intl = new Intl.DateTimeFormat([], {
-    dateStyle: 'medium',
-  });
+  const intl = new Intl.DateTimeFormat([], { dateStyle });
   return intl.format(parsed);
 };
