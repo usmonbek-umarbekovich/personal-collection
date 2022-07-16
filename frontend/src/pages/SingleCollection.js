0f9b1cd38b638e21f1b06faec7df5df4b0735a42
Usmonbek Rustamov <75022353+usmonbek-umarbekovich@users.noreply.github.com>
2022-07-16 23:41:24 +0200

    created SingleCollection page

--- frontend/src/pages/SingleCollection.js
+++ frontend/src/pages/SingleCollection.js
@@ -3,6 +3,7 @@ import collectionService from '../services/collectionService';
 import { useParams } from 'react-router-dom';
 import Items from '../components/Items';
 import Tags from '../components/Tags';
+import AuthorInfo from '../components/AuthorInfo';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 
@@ -11,19 +12,37 @@ function SingleCollection() {
   const { id } = useParams();
 
   useEffect(() => {
-    collectionService.getSingleCollection(id).then(data => setCollection(data));
+    collectionService.getSingleCollection(id).then(setCollection);
   }, [id]);
 
   if (!collection) return null;
 
   return (
-    <main className="py-lg-5 py-2 px-2">
-      <Container fluid="md">
-        <Row className="justify-content-between align-items-start">
-          <Tags callback={collectionService.getCollectionTags(id)} />
-          <Items callback={collectionService.getCollectionItems(id)} />
-        </Row>
-      </Container>
+    <main>
+      <section className="border-bottom py-lg-4 px-2">
+        <Container fluid="md">
+          <AuthorInfo
+            size="lg"
+            user={collection.user}
+            createdAt={collection.createdAt}
+          />
+          <h1 className="lh-base">{collection.name}</h1>
+          {collection.description && (
+            <p className="text-secondary fs-4">{collection.description}</p>
+          )}
+        </Container>
+      </section>
+      <section className="py-lg-5 px-2">
+        <Container fluid="md">
+          <Row className="justify-content-between align-items-start">
+            <Tags callback={collectionService.getCollectionTags(id)} />
+            <Items
+              inCollection={true}
+              callback={collectionService.getCollectionItems(id)}
+            />
+          </Row>
+        </Container>
+      </section>
     </main>
   );
 }
