67254aa1e8b52e17fc1d266562bea81be97e149e
Usmonbek Rustamov <75022353+usmonbek-umarbekovich@users.noreply.github.com>
2022-07-18 03:35:41 +0200

    implemented lastSeen helper

--- frontend/src/helpers.js
+++ frontend/src/helpers.js
@@ -14,3 +14,26 @@ export const formatTime = (rawTime, dateStyle) => {
   const intl = new Intl.DateTimeFormat([], { dateStyle });
   return intl.format(parsed);
 };
+
+export const lastSeen = time => {
+  if (!time) return 'Blocked';
+
+  const timeDiff = new Date() - new Date(time);
+
+  const minutes = Math.trunc(timeDiff / 1000 / 60);
+  if (minutes === 0) return 'Online';
+  if (minutes === 1) return 'Last Seen a minute ago';
+  if (minutes < 60) return `Last Seen ${minutes} minutes ago`;
+
+  const hours = Math.trunc(timeDiff / 1000 / 3600);
+  if (hours === 1) return 'Last Seen an hour ago';
+  if (hours < 24) return `Last Seen ${hours} hours ago`;
+
+  const days = Math.trunc(timeDiff / 1000 / 3600 / 24);
+  if (days === 1) return 'Last Seen a day ago';
+  if (days < 7) return `Last Seen ${days} days ago`;
+  if (days === 7) return 'Last Seen a week ago';
+
+  const date = formatTime(time, 'long');
+  return `Last seen on ${date}`;
+};
