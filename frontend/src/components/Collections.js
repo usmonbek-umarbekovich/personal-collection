1f11f610521baa80caa97b1b002dcb03a0eb9110
Usmonbek Rustamov <75022353+usmonbek-umarbekovich@users.noreply.github.com>
2022-07-18 03:37:37 +0200

    more generic Collections component

--- frontend/src/components/Collections.js
+++ frontend/src/components/Collections.js
@@ -1,70 +1,85 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useMemo } from 'react';
 import { Link } from 'react-router-dom';
-import collectionService from '../services/collectionService';
 import { formatTime } from '../helpers';
+import useLazyLoad from '../hooks/useLazyLoad';
 import AuthorInfo from './AuthorInfo';
-import Container from 'react-bootstrap/Container';
 import Stack from 'react-bootstrap/Stack';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Badge from 'react-bootstrap/Badge';
 import { FaCheckDouble } from 'react-icons/fa';
 
-function Collections() {
-  const [collections, setCollections] = useState([]);
+function Collections({
+  callback,
+  showUser,
+  indexes,
+  fill,
+  query,
+  topCollections,
+  root = '',
+}) {
+  // fill: true | false
+  const columns = {
+    true: { lg: 12 },
+    false: { sm: 6, lg: 4 },
+  };
+
+  const [skip, setSkip] = useState(0);
+  const params = useMemo(() => ({ skip, ...query }), [query, skip]);
+  const [collections, loading, hasMore] = useLazyLoad(params, callback);
 
   useEffect(() => {
-    collectionService
-      .getCollections({
-        limit: 6,
-        'meta.numItems': 'desc',
-      })
-      .then(data => {
-        setCollections(data);
-      });
-  }, []);
+    if (loading) return;
+    if (hasMore && !topCollections) setSkip(prevSkip => prevSkip + query.limit);
+  }, [loading, hasMore, query.limit, topCollections]);
 
   return (
-    <section className="border-bottom pt-lg-5 py-4 px-2">
-      <Container fluid="md">
-        <h2 className="fs-4">
+    <div>
+      {topCollections && (
+        <h2 className="fs-4 pb-3 pb-lg-4">
           <FaCheckDouble className="fs-5" /> Top Largest Collections
         </h2>
-        <Row as="ul" className="g-4 pt-3 pt-lg-4 px-0">
-          {collections.map((col, index) => (
-            <Col key={col._id} sm="6" lg="4" className="lh-1">
-              <Stack direction="horizontal" className="align-items-start gap-3">
+      )}
+      <Row as="ul" className="g-4 px-0">
+        {collections.map((col, index) => (
+          <Col key={col._id} {...columns[fill]} className="lh-1">
+            <Stack direction="horizontal" className="align-items-start gap-3">
+              {indexes && (
                 <p className="fs-3 fw-bold text-secondary opacity-75 mt-1">
                   0{index + 1}
                 </p>
+              )}
+              <Stack>
+                {showUser && (
+                  <AuthorInfo user={col.user} weight="bolder" root={root} />
+                )}
                 <Stack>
-                  <AuthorInfo user={col.user} size="sm" />
-                  <Stack>
-                    <Link to={`collections/${col._id}`} className="text-reset">
-                      <p className="fs-5 fw-bold">{col.name}</p>
-                      {col.description && (
-                        <p className="fs-5 lh-sm">{col.description}</p>
-                      )}
-                    </Link>
-                  </Stack>
-                  <Stack
-                    gap="2"
-                    direction="horizontal"
-                    className="align-items-start text-muted">
-                    <p>
-                      <Badge bg="info">{col.meta.numItems}</Badge> item
-                      {col.meta.numItems > 1 ? 's' : ''}
-                    </p>
-                    <p>-</p>
-                    <p>{formatTime(col.createdAt, 'medium')}</p>
-                  </Stack>
+                  <Link
+                    className="text-reset"
+                    to={`${root}/collections/${col._id}`}>
+                    <p className="fs-5 fw-bold">{col.name}</p>
+                    {col.description && (
+                      <p className="fs-5 lh-sm">{col.description}</p>
+                    )}
+                  </Link>
+                </Stack>
+                <Stack
+                  gap="2"
+                  direction="horizontal"
+                  className="align-items-start text-muted">
+                  <p>
+                    <Badge bg="info">{col.meta.numItems}</Badge> item
+                    {col.meta.numItems > 1 ? 's' : ''}
+                  </p>
+                  <p>-</p>
+                  <p>{formatTime(col.createdAt, 'medium')}</p>
                 </Stack>
               </Stack>
-            </Col>
-          ))}
-        </Row>
-      </Container>
-    </section>
+            </Stack>
+          </Col>
+        ))}
+      </Row>
+    </div>
   );
 }
 
