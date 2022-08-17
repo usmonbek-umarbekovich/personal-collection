506e70fb9a7fb999e6adcc1ce3e7afb904db8938
Usmonbek Rustamov <75022353+usmonbek-umarbekovich@users.noreply.github.com>
2022-08-17 19:33:49 +0200

    added Users component

---
+++ frontend/src/components/Users.js
@@ -0,0 +1,41 @@
+import { timeDiff, truncate } from '../helpers';
+import AuthorInfo from './AuthorInfo';
+import Stack from 'react-bootstrap/Stack';
+import Badge from 'react-bootstrap/Badge';
+
+function Users({ data = [] }) {
+  return (
+    <Stack gap="5">
+      {data.map(user => (
+        <Stack key={user._id}>
+          <AuthorInfo
+            fontSize="lg"
+            picSize="md"
+            weight="bolder"
+            user={user}
+            description={timeDiff(user.lastSeen, 'user', 'long')}
+          />
+          {user.bio && (
+            <p className="fs-5 lh-sm text-break">
+              {truncate(user.bio, 15, 180)}
+            </p>
+          )}
+          <Stack gap="4" direction="horizontal" className="fs-5">
+            <p>
+              <Badge bg="primary">{user.numItems}</Badge> item
+              {user.numItems > 1 ? 's' : ''}
+            </p>
+            <p>
+              <Badge bg="warning" text="dark">
+                {user.numCollections}
+              </Badge>{' '}
+              collection
+              {user.numCollections > 1 ? 's' : ''}
+            </p>
+          </Stack>
+        </Stack>
+      ))}
+    </Stack>
+  );
+}
+export default Users;
